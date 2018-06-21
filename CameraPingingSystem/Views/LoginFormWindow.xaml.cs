c4bfd7f5f72a065f03940e40763e1b51878ae1ee
Yusuf Mohamed <ymk.hassan@gmail.com>
2018-06-21 12:46:57 +0200

    Initial commit

---
+++ CameraPingingSystem/Views/LoginFormWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CameraPingingSystem.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginFormWindow.xaml
+    /// </summary>
+    public partial class LoginFormWindow : Window
+    {
+        public LoginFormWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.usernameTextBox.Clear();
+            this.passwordTextBox.Clear();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (this.usernameTextBox.Text.Length == 0 || this.passwordTextBox.Password.Length == 0)
+                MessageBox.Show(messageBoxText: "برجاء ادخال اسم المستخدم و/او كلمة السر", caption: "تسجيل الدخول");
+
+        }
+    }
+}
